a5b3306b75ccde3af0113b0ccd650919647faf48
Bawantha Mihiran Rajapaksha <bawwa.16@cse.mrt.ac.lk>
2024-10-29 07:39:26 +1100

    init

---
+++ shared/src/androidMain/kotlin/com/bawantha/dailypulse/Platform.android.kt
@@ -0,0 +1,7 @@
+package com.bawantha.dailypulse
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
